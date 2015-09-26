a60e676c7e05d3f8b8341e658b9cdea0ac7fc9a6
Olivia Nordquist <nolivia@umich.edu>
2015-09-26 16:35:00 -0700

    yelp integrated mostly

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify, request
+import yelp
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route("/")
 def index():
     return send_file("static/index.html")
 
+@app.route("/yelpmeup")
+def search():
+    term = request.args.get("term", "", type=str)
+    location = request.args.get("location", "", type=str)
+
+    return jsonify(**yelp.search(term, location))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
