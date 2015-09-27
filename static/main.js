860c7df7e646da6e49d9266c4d51845bac1ae8be
Olivia Nordquist <nolivia@umich.edu>
2015-09-26 20:18:02 -0700

    yelp On yelp functional

--- static/main.js
+++ static/main.js
@@ -5,24 +5,31 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
 }).addTo(map);
 $("#search").on("submit", function(event) {
   event.preventDefault();
+
   var term = $("[name=term]").val();
+  var multiple_terms = term.split(",");
   var location = $("[name=location]").val();
-  $.ajax({
-    data: {
-      term: term,
-      location: location
-    },
-    url: "/yelpmeup"
-  }).done(function(data) {
+
+  multiple_terms.forEach(function(term) {
+    $.ajax({
+      data: {
+        term: term,
+        location: location
+      },
+      url: "/yelpmeup"
+    })
+    .done(function(data) {
       var center = data.region.center;
       map.panTo({lng: center.longitude, lat: center.latitude});
 
       data.businesses.forEach(function(business) {
         var coord = business.location.coordinate;
         var marker = L.marker([coord.latitude, coord.longitude]).addTo(map);
+        marker.bindPopup("<b>" + business.name + "</b><br> Rating: " + business.rating + "<br><a href=" + business.url + ' target="_blank">See more on Yelp!').openPopup();
       });
     })
     .fail(function() {
       alert( "error" );
     });
+  });
 });
